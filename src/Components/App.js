bb8e41bd009160fef448c86446a4ff11404183aa
Vittman <Vladimir@MacBook-Pro-Nikolaj.local>
2020-05-13 20:41:06 +0300

    init commit

---
+++ src/Components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import GlobalStyles from "../Styles/GlobalStyles";
+
+export default () => (
+  <>
+    <GlobalStyles />
+    hello
+  </>
+)
---
+++ src/Styles/Theme.js
@@ -0,0 +1,8 @@
+export default {
+  bgColor: '#FAFAFA',
+  blackColor: '#262626',
+  blueColor: '#B9DFFC',
+  darkBlueColor: '#00366B',
+  boxBorder: '1px solid gray',
+  borderRadius: '4px'
+}
